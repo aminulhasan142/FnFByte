4ae255453b979270ab9977608156c6f2eea91559
aminulhasan142 <mahshoukhin142@gmail.com>
2024-12-18 22:01:53 +0600

    working on pricing section

--- src/main.js
+++ src/main.js
@@ -1,31 +1,31 @@
 // Swiper JS Initialize ---------------------------------------------------------------------- Swiper JS Initialize --------------------------------------
-const progressCircle = document.querySelector(".autoplay-progress svg");
-const progressContent = document.querySelector(".autoplay-progress span");
-var swiper = new Swiper(".heroSwiper", {
-  direction: getDirection(),
-  grabCursor: true,
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  on: {
-    autoplayTimeLeft(s, time, progress) {
-      progressCircle.style.setProperty("--progress", 1 - progress);
-      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
-    },
-  },
-});
-function getDirection() {
-  var windowWidth = window.innerWidth;
-  var direction = window.innerWidth <= 768 ? "horizontal" : "vertical";
+// const progressCircle = document.querySelector(".autoplay-progress svg");
+// const progressContent = document.querySelector(".autoplay-progress span");
+// var swiper = new Swiper(".heroSwiper", {
+//   direction: getDirection(),
+//   grabCursor: true,
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   on: {
+//     autoplayTimeLeft(s, time, progress) {
+//       progressCircle.style.setProperty("--progress", 1 - progress);
+//       progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+//     },
+//   },
+// });
+// function getDirection() {
+//   var windowWidth = window.innerWidth;
+//   var direction = window.innerWidth <= 768 ? "horizontal" : "vertical";
 
-  return direction;
-}
+//   return direction;
+// }
 
 //---------------------------------------------------------------- Swiper Intializer End----------------------------------------------
