e2bfa130dc31ee55bb3f7a03c13fc0cbf0f0ad25
aminulhasan142 <mahshoukhin142@gmail.com>
2024-12-19 23:08:52 +0600

    working on testimonial section

--- src/main.js
+++ src/main.js
@@ -1,31 +1,52 @@
 // Swiper JS Initialize ---------------------------------------------------------------------- Swiper JS Initialize --------------------------------------
-// const progressCircle = document.querySelector(".autoplay-progress svg");
-// const progressContent = document.querySelector(".autoplay-progress span");
-// var swiper = new Swiper(".heroSwiper", {
-//   direction: getDirection(),
-//   grabCursor: true,
-//   spaceBetween: 30,
-//   centeredSlides: true,
-//   autoplay: {
-//     delay: 2500,
-//     disableOnInteraction: false,
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   on: {
-//     autoplayTimeLeft(s, time, progress) {
-//       progressCircle.style.setProperty("--progress", 1 - progress);
-//       progressContent.textContent = `${Math.ceil(time / 1000)}s`;
-//     },
-//   },
-// });
-// function getDirection() {
-//   var windowWidth = window.innerWidth;
-//   var direction = window.innerWidth <= 768 ? "horizontal" : "vertical";
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+var slider1 = new Swiper(".slider1", {
+  direction: getDirection(),
+  grabCursor: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressCircle.style.setProperty("--progress", 1 - progress);
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    },
+  },
+});
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth <= 768 ? "horizontal" : "vertical";
 
-//   return direction;
-// }
+  return direction;
+}
 
 //---------------------------------------------------------------- Swiper Intializer End----------------------------------------------
+
+// swiper for textimonial
+
+var slider2 = new Swiper(".slider2", {
+  spaceBetween: 30,
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ export default {
         "border-primary": "#27272A",
         "border-brand": "#4F46E5",
         "border-color": "#ffffff4d",
+        "border-dark": "#3F3F46",
         "text-primary": "#A1A1AA",
         "text-secondary": "#E4E4E7",
         "nav-gradient-one": "#18191c",
@@ -21,12 +22,14 @@ export default {
         "text-muted": "#A1A1AA",
         "surface-brand": "#4F46E5",
         "surface-primary": "#1E293B",
+        "surface-background": "#18181B",
         "surface-brand-hover": "#4f46a8",
       },
       boxShadow: {
         "3xl": "0 5px 24px 0px rgba(0, 0, 0, 0.03)",
         "4xl": "0 5px 60px 10px rgba(11, 82, 235, 0.10)",
         "5xl": "0 26px 26px 0px rgba(7, 110, 154, 0.10)",
+        "6xl": "0px 3px 5px 0px rgba(30, 41, 59, 0.03)",
       },
       backgroundImage: {
         "hero-pattern": "url('/img/Line.svg')",
